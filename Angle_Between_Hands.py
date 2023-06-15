9c920655864f00a6e88bb972dec44f4379907074
Tarun <119723457+tarunnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn@users.noreply.github.com>
2023-06-15 10:09:28 +0530

    a,b=map(int,input().split(':')) angle=abs(5.5*b-30*a) if angle>180:     print(360-angle) else:     print(angle)

--- Angle_Between_Hands.py
+++ Angle_Between_Hands.py
@@ -1,17 +1,6 @@
-a=input()
-h=(ord(a[0])-48)*10+(ord(a[1])-48)
-m=(ord(a[3])-48)*10+(ord(a[4])-48)
-if m%2==0:
-    angle=30*h-((11*m*1.0)//2)
-    if angle<0:
-        angle+=360
-    if angle>180:
-        angle=360-angle
-    print("{:.1f}".format(angle))
+a,b=map(int,input().split(':'))
+angle=abs(5.5*b-30*a)
+if angle>180:
+    print(360-angle)
 else:
-    angle=(30*h*1.0)-(11*m*1.0/2)
-    if angle<0:
-        angle+=360
-    if angle>180:
-        angle=360-angle
-    print("{:.1f}".format(angle))
+    print(angle)
